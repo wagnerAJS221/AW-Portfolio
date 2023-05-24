db323c94a3512a14e806ac6db2dfa4f05979722c
wagnerAJS221 <wagneralex221@gmai.com>
2023-05-24 07:44:54 +0100

    npm start now working

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>This is the start of my portfolio</h1>
+      <h1 className="text 4xl font-bold">This is the start of my portfolio</h1>
     </div>
   )
 }
