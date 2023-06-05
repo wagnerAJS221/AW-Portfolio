f7fe079678e85754f5a762eebad78b5b4cb54c37
wagnerAJS221 <wagneralex221@gmai.com>
2023-06-05 07:12:14 +0100

    still cant get tailwind to work, errors not telling me much

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -3,8 +3,10 @@ import { FaBars, FaTimes } from 'react-icons/fa'
 
 const NavBar = () => {
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
-      <h1 className="text-5xl">Alex</h1>
+    <div className="flex justify-between items-center w-full h-20 text-white fixed">
+      <div>
+        <h1>Alex</h1>
+      </div>
     </div>
   )
 }
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,8 @@
+export const content = ['.src/**/*.{js,jsx.ts,tsx}']
+export const theme = {
+  extent: {},
+  fontFaimly: {
+    signature: ['Great Vibes']
+  }
+}
+export const plugins = []
